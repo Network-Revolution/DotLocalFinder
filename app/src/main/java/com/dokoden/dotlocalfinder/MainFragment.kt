ed97b02e2c0ca35d59a3f1f290196ef984d9e676
NetworkRevolution <netrevo818@gmail.com>
2020-06-14 23:57:13 +0900

    Use "by viewModels"
    Signed-off-by: NetworkRevolution <netrevo818@gmail.com>

--- app/src/main/java/com/dokoden/dotlocalfinder/MainFragment.kt
+++ app/src/main/java/com/dokoden/dotlocalfinder/MainFragment.kt
@@ -5,15 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dokoden.dotlocalfinder.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val mainViewModel =
-            ViewModelProvider.AndroidViewModelFactory(requireActivity().application).create(MainViewModel::class.java)
+        val mainViewModel by viewModels<MainViewModel>()
         val binding = MainFragmentBinding.inflate(inflater, container, false)
         val recyclerAdapter = MainRecyclerAdapter()
 
