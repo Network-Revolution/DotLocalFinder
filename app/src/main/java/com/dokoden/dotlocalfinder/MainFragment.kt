319957b1ab2cf478ef87368ad0e9e98ce53e59b3
NetworkRevolution <netrevo818@gmail.com>
2020-11-06 23:35:32 +0900

    Material Design Components updates
    Signed-off-by: NetworkRevolution <netrevo818@gmail.com>

--- app/src/main/java/com/dokoden/dotlocalfinder/MainFragment.kt
+++ app/src/main/java/com/dokoden/dotlocalfinder/MainFragment.kt
@@ -6,28 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.dokoden.dotlocalfinder.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val mainViewModel by viewModels<MainViewModel>()
-        val binding = MainFragmentBinding.inflate(inflater, container, false)
         val recyclerAdapter = MainRecyclerAdapter()
 
-        binding.viewModel = mainViewModel
-        binding.recyclerView.also {
-            it.adapter = recyclerAdapter
-            it.layoutManager = LinearLayoutManager(context)
-        }
-
-        mainViewModel.liveDataList.observe(viewLifecycleOwner, Observer {
+        mainViewModel.liveDataList.observe(viewLifecycleOwner, {
             it?.also {
                 recyclerAdapter.dataList = it
                 recyclerAdapter.notifyDataSetChanged()
             }
         })
-        return binding.root
+
+        MainFragmentBinding.inflate(inflater, container, false).also {
+            it.viewModel = mainViewModel
+            it.itemAdapter = recyclerAdapter
+            return it.root
+        }
     }
 }
--- app/src/main/java/com/dokoden/dotlocalfinder/MainRecyclerAdapter.kt
+++ app/src/main/java/com/dokoden/dotlocalfinder/MainRecyclerAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.dokoden.dotlocalfinder.databinding.MainRecyclerItemBinding
 
 class MainRecyclerAdapter : RecyclerView.Adapter<MainRecyclerAdapter.ViewHolder>() {
-    var dataList: List<MainDataClass> = emptyList()
+    var dataList = emptyList<MainDataClass>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
         MainRecyclerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
